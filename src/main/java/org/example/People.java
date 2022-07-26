21b358904c4663cf497d35cb66bbdd2ea9a2598c
Fatema Bohra <fatema.bohra2003@gmail.com>
2022-07-26 13:12:33 +0530

    Add the people.java file

---
+++ src/main/java/org/example/People.java
@@ -0,0 +1,91 @@
+package org.example;
+
+class Person{
+    // Classes can contain...
+    //Data
+    //Instance variable
+    String name;
+    int age;
+
+    //Methods (Subroutines)
+    void speak(String text){
+        System.out.println(text);
+    }
+
+    void yearsLeftToRetirement(){
+        int yearsLeft = 65 - age;
+        System.out.println("Years till retirement: " + yearsLeft);
+    }
+    //Another way to calculate yearsLeft
+    int calculateYearsLeftToRetirement(){
+        return 65 - age;
+    }
+
+    int getAge(){
+        return age;
+    }
+
+    String getName(){
+        return name;
+    }
+
+}
+
+class Robot{
+    public void speak(String word){
+        System.out.println(word);
+    }
+
+    public void jump(int height){
+        System.out.println("Jumping; " + height);
+    }
+
+    public void move(String direction, double distance){
+        System.out.println("moving " + distance + " metere in direction " + direction);
+    }
+}
+
+public class People {
+    public static void main(String[] args) {
+        Person person1 = new Person();
+        person1.name = "Bob Fanny";
+        person1.age = 37;
+
+        person1.speak("Im Bob");
+        person1.yearsLeftToRetirement();
+
+        //Another way to calculate yearsLeft
+        int years = person1.calculateYearsLeftToRetirement();
+        System.out.println("Years till retirement: " + years);
+
+        int ageP1 = person1.getAge();
+        System.out.println(ageP1);
+
+        String nameP1 = person1.getName();
+        System.out.println(nameP1);
+
+        Person person2 = new Person();
+        person2.name = "Sarah Hamir";
+        person2.age = 22;
+
+        person2.speak("Im Sarah");
+        person2.yearsLeftToRetirement();
+
+        //Another way to calculate yearsLeft
+        int year = person2.calculateYearsLeftToRetirement();
+        System.out.println("Years till retirement: " + year);
+
+        int ageP2 = person2.getAge();
+        System.out.println(ageP2);
+
+        String nameP2 = person2.getName();
+        System.out.println(nameP2);
+
+        //////////////////////////////////////////
+
+        Robot sam = new Robot();
+        sam.speak("Hi, I am Sam");
+        sam.jump(7);
+        sam.move("South", 45.3);
+    }
+}
